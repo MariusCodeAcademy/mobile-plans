220e24e985e2bb42d789e58208dcc525d48f0df9
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-19 13:27:13 +0300

    custom Radio

--- src/components/comitment.jsx
+++ src/components/comitment.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import CustomRadio from './common/customRadio';
 
 class Comitment extends Component {
+  handleRadio = () => {
+    console.log('radio was pressed');
+    // atvaizduoti 24 men. is CustomRadio komponento vidaus
+
+    // pakeisti teksta is not selected i selected kai paspaudziam ant mygtuko
+
+    // iskviesti handleRadio funkcija is CustomRadio componento
+
+    //
+  };
   render() {
     return (
       <div className="commitment-component">
         <div className="c__selection">
-          <span className="c__select"></span>
+          <span className="c__select">
+            <CustomRadio>24 men.</CustomRadio>
+          </span>
           <span className="c__select-title">24 men.</span>
         </div>
         <div className="c__selection">
