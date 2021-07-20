1348f2ecedb84045d7db5570382e50a981af569c
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-20 08:47:09 +0300

    mobile1 gets data on cdm, pass it as prop to MobilePlan

--- src/App.jsx
+++ src/App.jsx
@@ -9,36 +9,27 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      beIsipareiojimu: true,
+      beIsipareigojimu: true,
       mobile1: {},
     };
   }
-
-  // component did mount hook parsisiusnciam plan1.json faila ir issaugom state
-
-  // perduodam mobile1 i MobilePlan
-
-  // MobilePlan pasiimam duomenis ir uzpildom kortele
 
   handleRadio = (val) => {
     console.log('radio was pressed');
     console.log(val);
     // jei gaunu commit tai nustatau busena  i atitinkama
     if (val === 'commit') {
-      this.setState({ beIsipareiojimu: false });
+      this.setState({ beIsipareigojimu: false });
     } else {
-      this.setState({ beIsipareiojimu: true });
+      this.setState({ beIsipareigojimu: true });
     }
     // arba priesingai
   };
 
   async componentDidMount() {
-    const res = await fetch('/data/plan1.json');
-    const data = await res.json();
-    console.log(data);
-
     const { data: dataAxios } = await axios.get('data/plan1.json');
     console.log(dataAxios);
+    this.setState({ mobile1: dataAxios });
   }
 
   render() {
@@ -48,11 +39,11 @@ class App extends Component {
           <h1>Mobiliojo ryšio planai</h1>
           <p>Visos Lietuvoje turimo plano naudos galioja Baltijos ir Skandinavijos šalyse.</p>
           <div className="controls d-flex">
-            <Comitment handleRadio={this.handleRadio} noCommitment={this.state.beIsipareiojimu} />
+            <Comitment handleRadio={this.handleRadio} noCommitment={this.state.beIsipareigojimu} />
             <HaveServices />
           </div>
           <main className="plan-cards">
-            <MobilePlan />
+            <MobilePlan plan={this.state.mobile1} />
           </main>
         </div>
       </div>
